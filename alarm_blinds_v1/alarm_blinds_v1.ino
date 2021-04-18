449fa23a53b861820fa94671b676549c9ab715b3
kiribuilds@gmail.com <49044903+RoccoRuan@users.noreply.github.com>
2021-04-18 18:04:46 -0400

    Class-ified servo stuff

--- alarm_blinds_v1/alarm_blinds_v1.ino
+++ alarm_blinds_v1/alarm_blinds_v1.ino
@@ -1,6 +1,9 @@
 #include <Adafruit_Keypad.h>
 #include <Servo.h>
 
+//////////////////////////////////
+// Definitions
+//////////////////////////////////
 
 // ___________KEYPAD____________
 const byte ROWS = 4; // rows
@@ -19,16 +22,124 @@ byte colPins[COLS] = {4, 2, 6}; //connect to the column pinouts of the keypad
 Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 // __________SERVO______________
-Servo serv;
-const int servoPin = 11;
 
-#define DOWN 0
-#define UP 180
-#define STILL 90
 
 // __________DEBUG__________
 #define DEBUG true
 
+// __________SERVO STATE MACHINE_________
+
+class CBlindsServoSM {
+  public:
+    CBlindsServoSM(int iServoPin, int iPeriod);
+    ~CBlindsServoSM;
+
+    void SetState(EState state) {m_state = state;}
+    EState GetState() {return m_state};
+    void Update();
+
+  private:
+    Servo m_servo;
+    int m_iServoPin;
+    int m_iPeriod; // period to update with, in ms
+
+    // Position and direction info. Position measured in ms travelled in that direction.
+    int m_iCurrentPosition; // When iPosition is 0, blinds are down.
+    int m_iUpPosition; // When iPosition = iUpPosition, blinds are up.
+    int m_iServoDir; // Current direction servo is moving in 
+
+    const int m_iDownCmd;
+    const int m_iUpCmd;
+    const int m_iStillCmd;
+    int m_iCurrCmd;
+
+    enum EState {
+      NOT_MOVING,
+      AUTO_DOWN,
+      AUTO_UP,
+      AUTO_UP_WINDUP,
+      MANUAL_DOWN,
+      MANUAL_UP
+    }
+    EState m_state;
+}
+
+CBlindsServoSM::CBlindsServoSM(int iServoPin, int iPeriod)
+{
+  m_iServoPin = iServoPin;
+  m_iPeriod = iPeriod;
+
+  m_state = NOT_MOVING;
+
+  m_iDownCmd = 0;
+  m_iUpCmd = 180;
+  m_iStillCmd = 90;
+  m_iCurrCmd = m_iStillCmd;
+
+  m_iCurrentPosition = 0;
+  m_iUpPosition = 0;
+  m_iServoDir = m_iUpCmd;
+
+  m_servo = new Servo();
+  m_servo.attach(iServoPin);
+  m_servo.write(iStillCmd);
+}
+
+~CBlindsServoSM::CBlindsServoSM()
+{
+  delete m_servo;
+}
+
+CBlindsServoSM::Update()
+{
+  switch(m_state)
+  {
+    case(NOT_MOVING):
+      m_iCurrCmd = m_iStillCmd;
+      break;
+    case(AUTO_DOWN):
+      m_iCurrCmd = m_iDownCmd;
+      if(m_iCurrentPosition <= 0)
+      {
+        SetState(NOT_MOVING);
+        break;
+      }
+    case(AUTO_UP):
+      m_iCurrCmd = m_iUpCmd;
+      if(m_iCurrentPosition >= m_iUpPosition)
+      {
+        SetState(NOT_MOVING);
+        break;
+      }
+    case(AUTO_UP_WINDUP):
+      m_iCurrCmd = m_iDownCmd;
+      if(m_iCurrentPosition <= -500)
+      {
+        SetState(AUTO_UP)
+      }
+      break;
+    case(MANUAL_DOWN):
+      m_iCurrCmd = m_iDownCmd;
+      break;
+    case(MANUAL_UP):
+      m_iCurrCmd = m_iUpCmd;
+      break;
+  }
+
+  m_servo.write(m_iCurrCmd);
+  if (m_iCurrCmd == m_iDownCmd)
+  {
+    m_iCurrentPosition -= m_iPeriod;
+  }
+  if (m_iCurrCmd == m_iUpCmd)
+  {
+    m_iCurrentPosition += m_iPeriod;
+  }
+}
+
+//////////////////////////////////
+// Setup
+//////////////////////////////////
 void setup() {
 // ___________KEYPAD____________
   customKeypad.begin();
@@ -41,6 +152,9 @@ void setup() {
   if (DEBUG) {Serial.begin(9600);}
 }
 
+//////////////////////////////////
+// Loop
+//////////////////////////////////
 void loop() {
   // put your main code here, to run repeatedly:
   customKeypad.tick();
@@ -49,18 +163,21 @@ void loop() {
     keypadEvent e = customKeypad.read();
     if(e.bit.EVENT == KEY_JUST_RELEASED) {
       switch ((char)e.bit.KEY){
+        // Move blinds to "down" position
         case '*':
-          serv.write(DOWN);
           if (DEBUG) {Serial.println("Down");}
           break;
+        // Stop blinds
         case '0':
-          serv.write(STILL);
           if (DEBUG) {Serial.println("Still");}
           break;
+        // Move blinds to "up" position
         case '#':
-          serv.write(UP);
           if (DEBUG) {Serial.println("Up");}
           break;
+        // Set down (reference position)
+        case '1':
+          iCurrentPosition = 0;
         default:
           if (DEBUG) {Serial.println("Unrecognized");}
           break;
@@ -68,5 +185,14 @@ void loop() {
     }
   }
 
-  delay(10);
+  serv.write(iServoDir);
+  if(iServoDir == UP)
+  {
+    iCurrentPosition += iPeriod;
+  }
+  if(iServoDir == DOWN)
+  {
+    iCurrentPosition -= iPeriod;
+  }
+  delay(iPeriod);
 }
